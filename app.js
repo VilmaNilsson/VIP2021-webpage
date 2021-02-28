3a2096433bc79fbd4436fed6ad170b787351bb38
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-02-28 13:52:22 +0100

    Port numbers
    Added support for custom port numbers.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+// Makes us able to run with a custom port via the terminal, eg:
+//  $ PORT=8080 node app.js
+// Defaults to 3000.
 const port = 3000;
 
 // The templating engine Pug

14c53e7b23ff4be5efc1cff10d0705488533576b
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-02-28 13:43:22 +0100

    Serve static files
    Added support for serving static files. Placed within the "public" folder and served via the URI "/static". Added a CSS and JS file as examples.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Dependencies
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -7,10 +9,14 @@ const port = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+// Serve static files from the folder `public`, but by the URI "/static"
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// Start our server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });

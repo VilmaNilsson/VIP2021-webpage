eaf5b1e64feaeae4947663b71c88daf80b54f11e
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-02-28 14:18:48 +0100

    Support for 404/500
    Added error pages for 404 and 500.

--- app.js
+++ app.js
@@ -3,26 +3,64 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express();
 // Makes us able to run with a custom port via the terminal, eg:
 //  $ PORT=8080 node app.js
-// Defaults to 3000.
+// Defaults to 3000
 const port = 3000;
+const app = express();
+
+// SETTINGS
+// ========
 
 // The templating engine Pug
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+// MIDDLEWARE
+// ==========
 
 // Serve static files from the folder `public`, but by the URI "/static"
 app.use('/static', express.static(path.join(__dirname, 'public')));
 // HTTP logging
 app.use(morgan('combined'));
 
+// ROUTES
+// ======
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// ERROR HANDLERS
+// ==============
+
+// Pages not found will be routed into this function
+app.use(function(req, res, next){
+  res.status(404);
+
+  // Based on the requested format we can serve the user different types of
+  // representations
+  res.format({
+    html: function () {
+      res.render('404', { url: req.url });
+    },
+    json: function () {
+      res.json({ error: 'Not found' });
+    },
+    default: function () {
+      res.type('txt').send('Not found');
+    }
+  });
+});
+
+// If an error occurs in one of our routing functions we'll be routed here
+// instead, so the user doesn't get more information about what went wrong
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  res.render('500', { error: err });
+});
+
 // Start our server
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`[SERVER]: Started listening at http://localhost:${port}`);
 });

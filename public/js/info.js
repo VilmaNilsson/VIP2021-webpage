637bc6bad68a2f1302de655741fe2d66bdc3c120
Louise Edström <louise.edstrom98@gmail.com>
2021-03-04 08:53:29 +0100

    Infopage created
    The infopage has some placeholders for questions and does not yet include the navigation as it does not yet exist.
    
    This is a WIP, as the client has to communicarte with app.js
    
    I'm also just trying to get the pull-request working :')

--- public/js/info.js
+++ public/js/info.js
@@ -1,1 +1,52 @@
-console.log('Welcome to the infopage');
+"use strict"
+//will we be using strict?
+
+const qa = document.querySelectorAll('.QA');
+qa.forEach(element => {
+  element.addEventListener('click', () => {
+    const answer = element.childNodes[1];
+
+    if(answer.classList.contains('answerClose')){
+      answer.classList.add('answerOpen');
+      answer.classList.remove('answerClose');
+    } else {
+      answer.classList.add('answerClose');
+      answer.classList.remove('answerOpen');
+    }
+  });
+});
+
+const sendButton = document.querySelector('#sendButt');
+
+sendButton.addEventListener('click', () => {
+  let errorMessage = document.querySelector('.hiddenMessage');
+  if(errorMessage){
+    errorMessage.classList.remove('errorMessage');
+    errorMessage.classList.add('hiddenMessage');
+  }
+  const name = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const message = document.querySelector('#message').value;
+
+  if(name === '' || email === '' || message == ''){
+    errorMessage.classList.add('errorMessage');
+    errorMessage.classList.remove('hiddenMessage');
+    return;
+  }
+
+
+  
+  const info = {
+    name,
+    email,
+    message
+  };
+
+  console.log(info);
+
+  const serverInfo = JSON.stringify(info);
+
+  console.log(serverInfo);
+  // POST
+  // JSON => send to server 
+});

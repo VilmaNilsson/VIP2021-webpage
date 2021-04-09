0bad067ab0a706d818c31599fc9bf2aaf60d5209
VilmaNilsson <vilma.paulina.nilsson@hotmail.com>
2021-04-09 14:57:47 +0200

    Version 2.10

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,13 +1,13 @@
 //works without, is this needed?
-// const filepath = window.location.pathname;
-// const navLinks = document.querySelectorAll('.navLink');
-//
-// navLinks.forEach((link) => {
-//   if (filepath === link.getAttribute('href')) {
-//     console.log(`Welcome to page ${link.getAttribute('href')}`);
-//     link.classList.add('currentPage');
-//   }
-// });
+const filepath = window.location.pathname;
+const navLinks = document.querySelectorAll('.navLink');
+
+navLinks.forEach((link) => {
+  if (filepath === link.getAttribute('href')) {
+    console.log(`Welcome to page ${link.getAttribute('href')}`);
+    link.classList.add('currentPage');
+  }
+});
 
 document.getElementById('navHamContainer').onclick = function(){
   this.classList.toggle('change');

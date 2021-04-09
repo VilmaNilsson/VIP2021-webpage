c445e481d7bafdcacd2987b19ad54e1820db87f6
VilmaNilsson <vilma.paulina.nilsson@hotmail.com>
2021-04-09 15:00:57 +0200

    Version 2.12

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,14 +1,3 @@
-//works without, is this needed?
-const filepath = window.location.pathname;
-const navLinks = document.querySelectorAll('.navLink');
-
-navLinks.forEach((link) => {
-  if (filepath === link.getAttribute('href')){
-    console.log (`Welcome to page ${link.getAttribute('href')}`);
-    link.classList.add ('currentPage');
-  }
-});
-
 document.getElementById('navHamContainer').onclick = function(){
   this.classList.toggle('change');
   document.getElementById('navGrid').classList.toggle('navGridOpenClose');

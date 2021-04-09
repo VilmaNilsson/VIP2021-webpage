9aeddbeb1e52caea75c0a884c3537841eccc8b3b
VilmaNilsson <vilma.paulina.nilsson@hotmail.com>
2021-04-09 15:03:41 +0200

    Update navigation.js

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,3 +1,14 @@
+//works without, is this needed?
+// const filepath = window.location.pathname;
+// const navLinks = document.querySelectorAll('.navLink');
+//
+// navLinks.forEach((link) => {
+//   if (filepath === link.getAttribute('href')) {
+//     console.log(`Welcome to page ${link.getAttribute('href')}`);
+//     link.classList.add('currentPage');
+//   }
+// });
+
 document.getElementById('navHamContainer').onclick = function(){
   this.classList.toggle('change');
   document.getElementById('navGrid').classList.toggle('navGridOpenClose');

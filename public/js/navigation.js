88fe0a55ec3ff2aedd78582d887320e8dbf80fff
Petter Ekedahl <petter.ekedahl@gmail.com>
2021-04-12 21:13:29 +0200

    Added JS functions and css
    Deleted some of the Links

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -9,7 +9,25 @@
 //   }
 // });
 
+const about = document.querySelector('.navAbout');
+const events = document.querySelector('.navEvents');
+const theGame = document.querySelector('.navGame');
+
+let isAboutShowing = false;
+let isEventsShowing = false;
+let isTheGameShowing = false;
+
 document.getElementById('navHamContainer').onclick = function () {
   this.classList.toggle('change');
   document.getElementById('navGrid').classList.toggle('navGridOpenClose');
 };
+
+function displayNavDiv(divClass) {
+  const navDiv = document.querySelector(divClass);
+
+  navDiv.style.display = 'flex';
+}
+
+about.addEventListener('click', () => {
+  displayNavDiv('.aboutDiv');
+});

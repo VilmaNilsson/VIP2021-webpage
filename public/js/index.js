b86ed4156f66f8eaed574a810fcd588804fe5358
MeliDeu <m.deuretzbacher@yahoo.se>
2021-04-15 00:35:40 +0200

    Index_cleanUp#68 as well as randomizePlanets#76
    I missed that the randomizing function had its own issue, so i made both in the same branch. Some things need to be changed: update the list of planets in index.js (name, imgUrl and link to page), even the information text in the last div misses text atm

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,66 @@
-// Each page can have their own JS
+/* eslint-disable linebreak-style */
+// array with all the planets and pictures --> needs update when planets ready
+const planets = [
+  {
+    name: 'Pluto',
+    imgUrl: 'static/images/Pluto-15.svg',
+    link: '#Pluto'
+  },
+  {
+    name: 'Moo',
+    imgUrl: 'static/images/Moo-15.svg',
+    link: '#Moo'
+  },
+  {
+    name: 'Uranus',
+    imgUrl: 'static/images/Pluto-15.svg',
+    link: '#Uranus'
+  },
+  {
+    name: 'Urfanny',
+    imgUrl: 'static/images/Moo-15.svg',
+    link: '#Urfanny'
+  },
+  {
+    name: 'Urbum',
+    imgUrl: 'static/images/Pluto-15.svg',
+    link: '#Urbum'
+  },
+  {
+    name: 'Urparts',
+    imgUrl: 'static/images/Moo-15.svg',
+    link: '#Urparts'
+  }
+];
+
+// randomize then 3 planets for each reload
+// randomNumber-function is placed in utils.js
+const randomPlanets = [];
+
+for (let i = 0; i < 3; i++) {
+  const randomNr = randomNumber(0, planets.length);
+  randomPlanets.push(planets[randomNr]);
+  planets.splice(randomNr, 1);
+}
+
+// make elements for these in the planetsContainer on the index-page
+let planetWrapper = document.getElementById('indexWrapperDivPlanets');
+
+// function for the HTML-elements for the planets
+function createIndexPlanets(arrEl) {
+  let planetDiv = document.createElement('a');
+  planetDiv.classList.add('indexPlanetDiv');
+  planetDiv.setAttribute('href', arrEl.link);
+  let planetImg = document.createElement('div');
+  planetImg.classList.add('indexPlanetImg');
+  planetImg.style.backgroundImage = `url(${arrEl.imgUrl})`;
+  let planetName = document.createElement('p');
+  planetName.innerText = arrEl.name;
+  planetDiv.append(planetImg, planetName);
+  return planetDiv;
+}
+
+// place it in the container
+randomPlanets.forEach((rdmPlanet) => {
+  planetWrapper.append(createIndexPlanets(rdmPlanet));
+});
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,5 +1,11 @@
+/* eslint-disable linebreak-style */
 // We'll store common utility functions within this file
 
 // function byId(id) {
 //   return document.getElementById(id);
 // }
+
+// random number (max not included)
+function randomNumber(min, max) {
+  return min + Math.floor((max - min) * Math.random());
+}

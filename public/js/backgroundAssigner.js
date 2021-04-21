4ecd90e4a9377157a7dd4c32d03886a3ad6d5486
LudvigStridsberg <77850704+LudvigStridsberg@users.noreply.github.com>
2021-04-21 17:55:21 +0200

    Fixed styling-errors
    Fixed the styling-errors that showed up.

--- public/js/backgroundAssigner.js
+++ public/js/backgroundAssigner.js
@@ -1,14 +1,14 @@
 // Create a new HTMLCollection that will hold all elements that has the class
-let planetItemArray = document.getElementsByClassName("starBackgound");
-
-// Loop through the collection and give one randomly chosen backround to each element
-for (let i = 0; i < planetItemArray.length; i++) {
-    planetItemArray[i].style.backgroundImage = "url(/static/images/backgrounds/Background" + randomNumber() + "-18.png)";
-}
+const planetItemArray = document.getElementsByClassName('starBackgound');
 
 // Function for generating a random number
 function randomNumber() {
-    let min = Math.ceil(1);
-    let max = Math.floor(5);
-    return Math.floor(Math.random() * (max - min) + min);
+  const min = Math.ceil(1);
+  const max = Math.floor(5);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// Loop through the collection and give one randomly chosen backround to each element
+for (let i = 0; i < planetItemArray.length; i += 1) {
+  planetItemArray[i].style.backgroundImage = `url(/static/images/backgrounds/Background${randomNumber()}-18.png)`;
 }

1ae466d73b558b365662ff08912be153c5f764cd
kojala <kajsaojala@hotmail.com>
2021-05-06 16:39:15 +0200

    Small fixes around the page
    - Fixed the different fonts in about planets/crews
    - Adjusted size of elements on about us page
    - Button to show more updates from team
    - Neon borders fixed

--- public/js/index.js
+++ public/js/index.js
@@ -80,3 +80,20 @@ function createIndexPlanets(arrEl) {
 randomPlanets.forEach((rdmPlanet) => {
   planetWrapper.append(createIndexPlanets(rdmPlanet));
 });
+
+
+
+// Click event for "see more updates from team"-button
+let showBtn = document.getElementById('toggle-button')
+let showBtnText = document.getElementById('toggle-button').innerText;
+
+document.querySelector('#toggle-button').addEventListener('click', function() {
+  document.querySelector('.teamUpdateDivOld.collapsible').classList.toggle('collapsed');
+
+  if (showBtn.innerText == showBtnText){
+    showBtn.innerText = "Show less updates";
+  } else {
+    showBtn.innerText = showBtnText;
+  }
+
+});
